0a32ead5e742c24c56be373bbd067f212807ba9c
Jason Parker <north@ntbox.com>
2026-07-09 02:16:21 -0400

    Fix path when running directly from node.

--- scripts/build.ts
+++ scripts/build.ts
@@ -34,6 +34,10 @@ async function main() {
         build({
             configFile: false,
             mode: 'production',
+            define: {
+                'process.env.WS_NO_BUFFER_UTIL': '1',
+                'process.env.WS_NO_UTF_8_VALIDATE': '1',
+            },
             ssr: {
                 noExternal: true,
             },
--- src/server/paths.ts
+++ src/server/paths.ts
@@ -1,6 +1,7 @@
 import os from 'node:os';
 import path from 'node:path';
 import process from 'node:process';
+import { fileURLToPath } from 'node:url';
 
 export function getUserAppRoot(): string {
     return path.join(os.homedir(), '.romrack');
@@ -10,6 +11,11 @@ export function getAppRoot(): string {
     const appRoot = process.env.APP_ROOT;
     if (appRoot && appRoot.length > 0) {
         return path.resolve(appRoot);
+    }
+
+    const dir = path.dirname(fileURLToPath(import.meta.url));
+    if (path.basename(dir) === 'server') {
+        return path.resolve(dir, '..');
     }
 
     return process.cwd();
